20a09bf6b3d21259e129bf9fd3e5182c2a3b2dfd
Sourabh7singh <souravhsingh2002@gmail.com>
2025-02-25 14:30:12 +0000

    Code challenge submission for question: 268 Missing Number

---
+++ 268 Missing Number/268missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        for(int i=0;i<=nums.size();i++){
+            sum+=i;
+        }
+
+        int secondSum = 0;
+        for(int i=0;i<nums.size();i++){
+            secondSum+=nums[i];
+        }
+
+        return abs(sum-secondSum);
+        
+    }
+};
