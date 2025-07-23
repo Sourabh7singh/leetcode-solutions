6f2ac3210e89df63e5c5a6ba379922c84c7a6a00
Sourabh7singh <souravhsingh2002@gmail.com>
2025-07-23 15:57:07 +0000

    Code challenge submission for question: 374 Guess Number Higher or Lower

---
+++ 374 Guess Number Higher or Lower/374guess-number-higher-or-lower.cpp
@@ -0,0 +1,31 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        int mid = start + (end-start)/2;
+        int guessed_number;
+        while(start<=end){
+            guessed_number = guess(mid);
+            if(guessed_number==0){
+                break;
+            }else if(guessed_number==1){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+            mid = start + (end-start)/2;
+        }
+
+        return mid;
+    }
+};
