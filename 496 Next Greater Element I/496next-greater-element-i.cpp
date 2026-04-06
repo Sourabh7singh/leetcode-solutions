6aee8e80cd6f9638d2b5b303d8f00886d7343adc
Sourabh7singh <souravhsingh2002@gmail.com>
2026-04-06 16:54:25 +0000

    Code challenge submission for question: 496 Next Greater Element I

---
+++ 496 Next Greater Element I/496next-greater-element-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mapping;
+        stack<int> st;
+
+        for (int i = nums2.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            mapping[nums2[i]] = st.empty() ? -1 : st.top();
+            st.push(nums2[i]);
+        }
+
+        vector<int> answer(nums1.size());
+
+        for (int i = 0; i < nums1.size(); i++) {
+            answer[i] = mapping[nums1[i]];
+        }
+
+
+        return answer;
+    }
+};
